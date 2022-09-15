818c47c3c0bf98bab001070ae139bf95da1ffe76
msatchell1 <112362230+msatchell1@users.noreply.github.com>
2022-09-15 09:57:44 -0400

    created weather_prediction

---
+++ michael_code/funit.m
@@ -0,0 +1,15 @@
+% Defines the class funit (firing unit) as the class of firing unit
+% objects. It holds all properties of units.
+
+classdef funit
+
+    properties
+        
+        ID
+        rate
+        name
+        I
+
+    end
+
+end
