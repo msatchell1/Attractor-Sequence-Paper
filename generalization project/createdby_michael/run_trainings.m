b2e6b024f21e6cc4dc64b852c05723ad494c872e
msatchell1 <112362230+msatchell1@users.noreply.github.com>
2022-10-06 12:50:56 -0400

    update

--- generalization project/createdby_michael/run_trainings.m
+++ generalization project/createdby_michael/run_trainings.m
@@ -33,6 +33,7 @@ for i = 1:size(tp.type_combs,2)
         % Remember that clsf_index_array holds the classifications for the
         % two SVM models that will be compared later on with a dotp.
         tp.clsf_index_array = [tp.type_combs(:,i),tp.type_combs(:,j)];
+        
         [LCOs] = Train_LinClass(tp); % Runs simulations.
 
         W1 = LCOs(1).Mdl.Beta; W2 = LCOs(2).Mdl.Beta;
@@ -44,7 +45,7 @@ for i = 1:size(tp.type_combs,2)
         if tp.type_combs(1,i) == tp.type_combs(1,j) % if chars are equal
 
             if tp.type_combs(2,i) == tp.type_combs(2,j) % if types are equal
-               dotp_same_chartype(end+1) = dotp; 
+               dotp_same_chartype(end+1) = dotp;
             else % if types are not equal
                 dotp_same_char(end+1) = dotp;
             end
@@ -69,5 +70,6 @@ plot(zeros(length(dotp_diff_char),1)+3, dotp_diff_char, 'ro')
 set(gca,'xtick',[1 2 3]);
 xticklabels(["Same char/type", "Same type only", "Different char"]);
 ylabel("Dot Product")
+title(strcat("stim seeds = ",num2str(tp.stim_seeds)));
 xlim([0,4]);
 
